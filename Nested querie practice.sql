cfed8a3a125df92fa6c8df307425b65ddde6817e
Alexis Barrett <happynessb09@gmail.com>
2026-06-03 10:55:32 -0400

    Watched the instructor demonstrate question 3 in a demo

--- Nested querie practice.sql
+++ Nested querie practice.sql
@@ -1,4 +1,18 @@
 -- nested-queries
+use northwind;
 -- select productID, productName, unitPrice 
 -- from products
 -- where unitPrice = (select max(unitPrice)from products);
+
+-- select orderID, shipName, shipAddress 
+-- from orders where shipvia = (select shipperID from shippers
+-- where companyName = 'Federal shipping');
+
+/*
+
+select orderID from `order details`
+where productID =
+(select productId from products 
+where productName = 'Sasquatch Ale');
+
+*/
